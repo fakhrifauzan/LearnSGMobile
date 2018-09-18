f9984d7b2a21b91925608b7995702901862d7dff
Fakhri Fauzan <fazan697@gmail.com>
2018-09-18 20:07:52 +0700

    add butterknife

--- app/src/main/java/id/my/fazan/learnsgmobile/MainActivity.java
+++ app/src/main/java/id/my/fazan/learnsgmobile/MainActivity.java
@@ -5,45 +5,43 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import id.my.fazan.learnsgmobile.model.User;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.til_nama)
     TextInputLayout til_nama;
+
+    @BindView(R.id.et_umur)
     EditText et_umur;
+
+    @BindView(R.id.spinner_gender)
     Spinner spinner_gender;
+
+    @BindView(R.id.btn_proses)
     Button btn_proses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        til_nama = findViewById(R.id.til_nama);
-        et_umur = findViewById(R.id.et_umur);
-        spinner_gender = findViewById(R.id.spinner_gender);
-        btn_proses = findViewById(R.id.btn_proses);
-
-        btn_proses.setOnClickListener(this);
     }
 
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_proses:
-                validasiData();
-                break;
-        }
-    }
-
+    @OnClick(R.id.btn_proses)
     void validasiData(){
         if (til_nama.getEditText() != null){
             // Jika Nama dan Umur tidak kosong
